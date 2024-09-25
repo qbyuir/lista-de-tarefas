9e392f85bb51fa4ec840ba3e250f92c1913e70c2
yuriq <yuriqbarbosa@gmail.com>
2024-09-25 15:03:16 -0300

    nao sei

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import Container from './components/Container'
 
 const App = ()  => {
   return (
-    // container - h1, div container, input, button
-    // AddTask - function to add tasks
+    
     <main>
       <h1 className='text-4xl text-center py-28 text-white drop-shadow-txt-shadow'>Lista de Tarefas</h1>
       <Container />
